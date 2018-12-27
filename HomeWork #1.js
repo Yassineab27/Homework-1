ed61e526abebdf54308a2903eec30ced088a8240
Yassineab27 <45517237+Yassineab27@users.noreply.github.com>
2018-12-27 14:14:18 +0100

    Create HomeWork #1.js

---
+++ HomeWork #1.js
@@ -0,0 +1,13 @@
+const band = "ACDC";
+const yearOfCreation = 1973;
+const genre = "Hard Rock";
+const origin = "Australia"
+const members = ["Angus Young", "Brian Johnson", "Bon Scott"];
+const awards = "Grammy Hall of Fame Award";
+
+console.log(band); // ACDC
+console.log(yearOfCreation); //1973
+console.log(genre); // Hard Rock
+console.log(origin); // Australia
+console.log(members); // 0: "Angus Young" 1: "Brian Johnson" 2: "Bon Scott"
+console.log(awards);
